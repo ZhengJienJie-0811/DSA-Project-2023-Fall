2b916d864262736bba9c00b97642e5a0e1aba4dd
James-1723 <69380745+James-1723@users.noreply.github.com>
2024-01-03 11:45:28 +0800

    local change

--- DS_final_1225/.history/src/Main_20231229002057.java
+++ DS_final_1225/.history/src/Main_20231229002057.java
@@ -12,13 +12,13 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class Main {
-
+public class Main{
+	
 	// testing!
 	public static void main(String[] args) {
-
+	
 		// 讀取輸入的3個Keyword，並建立Keyword物件設定權重分別為5,3,1
-		Scanner input = new Scanner(System.in);
+		Scanner input = new Scanner(System.in);	
 		String k1 = input.next();
 		String k2 = input.next();
 		String k3 = input.next();
@@ -29,42 +29,42 @@ public class Main {
 		keywords.add(keyword_1);
 		keywords.add(keyword_2);
 		keywords.add(keyword_3);
-
-		// 利用googleQuery找尋有關主題-->Clothes的網頁
+		
+		//利用googleQuery找尋有關主題-->Clothes的網頁
 		GoogleQuery ClothesQuery = new GoogleQuery("Clothes");
 		WebPage root = new WebPage("http://soslab.nccu.edu.tw/Welcome.html", "searchResult");
 		WebTree tree = new WebTree(root);
-
+		
 		try {
-			ClothesQuery.query();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+				ClothesQuery.query();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		
 		ArrayList<WebPage> pageArr = ClothesQuery.getPagelist();
-
-		// for(WebPage wp:pageArr) {
-		// String decode_url = "";
-		// try {
-		// decode_url = java.net.URLDecoder.decode(wp.url,"utf-8");
-		// } catch (UnsupportedEncodingException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// tree.root.addChild(new WebNode(new WebPage(decode_url, wp.name)));
-		// }
-
-		for (int i = 0; i < 4; i++) {
+		
+//		for(WebPage wp:pageArr) {		
+//			String decode_url = "";
+//			try {
+//				decode_url = java.net.URLDecoder.decode(wp.url,"utf-8");
+//			} catch (UnsupportedEncodingException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}	
+//			tree.root.addChild(new WebNode(new WebPage(decode_url, wp.name)));
+//		}
+		
+		for(int i = 0 ; i < 4 ; i++) {	
 			tree.root.addChild(new WebNode(new WebPage(pageArr.get(i).url, pageArr.get(i).name)));
 			ArrayList<WebNode> root_child_child = new ArrayList<WebNode>();
 			root_child_child = ClothesQuery.findChildUrl(pageArr.get(i).url);
-			for (int j = 0; j < root_child_child.size(); j++) {
-				// tree.root.children.get(i).children.add(new WebNode(new
-				// WebPage("https://tw.dictionary.search.yahoo.com/;_ylt=Awrtkq3ZaY1lSWsJ2aN9rolQ","TEST")));
+			for(int j = 0; j < root_child_child.size(); j++) {
+				//tree.root.children.get(i).children.add(new WebNode(new WebPage("https://tw.dictionary.search.yahoo.com/;_ylt=Awrtkq3ZaY1lSWsJ2aN9rolQ","TEST")));
 				tree.root.children.get(i).children.add(root_child_child.get(j));
 			}
 		}
-
+		
+		
 		try {
 			tree.setPostOrderScore(keywords);
 		} catch (IOException e) {
@@ -73,7 +73,7 @@ public class Main {
 		}
 		tree.eularPrintTree();
 		System.out.println("FINISHED");
-
+		
 		input.close();
 	}
 }
